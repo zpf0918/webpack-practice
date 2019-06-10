1360a5e183b80e42fed26dd781e99727f2090b84
kafeidebai <zhanggongzifei@163.com>
2019-06-10 15:50:22 +0800

    require 的灵活性实践

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,16 @@
 const webpack = require("webpack")
 const HtmlWebpackPlugin = require("html-webpack-plugin")
 const webpackMerge = require('webpack-merge')
-const modeConfig = env => require(`./build-utils/webpack.${env}`)(env)
+const modeConfig = env => require(`./build-utils/webpack.${env}.js`)(env)
 
 module.exports = ({ mode, presets } = { mode: 'production', presets: [] }) => {
   return webpackMerge({
     mode,
     output: {
-      filename: 'bundle-test.js'
+      filename: 'bundle.js'
     },
     plugins: [new HtmlWebpackPlugin(), new webpack.ProgressPlugin()]
   },
-    modeConfig({ mode, presets}) 
+  modeConfig(mode)
   )
 }
