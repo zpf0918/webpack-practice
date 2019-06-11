429efd5cc007672c3138fcb4a6ef5dcf6b9a38f6
kafeidebai <zhanggongzifei@163.com>
2019-06-11 15:09:00 +0800

    bundle analyzer presets

--- src/foo.ts
+++
@@ -1,6 +0,0 @@
-export default class foo {
-  public bar
-  constructor () {
-    this.bar = 'hi'
-  }
-}
--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 import './footer.css'
 import './button.css'
-import { top, bottom, footer } from './footer'
-import nav from './nav'
-import makeButton from './button'
-import { red, blue } from './button-styles'
+import { footer } from './footer.js'
+import makeButton from './button.js'
+import { red } from './button-styles.js'
 import imgUrl from './webpack.logo.jpeg'
-import makeImage from './image'
-import Foo from './foo.ts'
+import makeImage from './image.js'
 
 const button = makeButton('Yay! My button')
 const image = makeImage(imgUrl)
